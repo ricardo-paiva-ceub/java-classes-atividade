f6614e000501a9942bdc42cb19c1a892f61921ce
ricardo_paiva <ricardo.paiva@sempreceub.com>
2021-11-16 18:50:42 -0300

    Adicionada funcionalidade - manipulação de exceções na tela de inclusão de novas roupas.

---
+++ trabalhoJavaVestuario/docs/listaImplementacoes.md
@@ -0,0 +1,20 @@
+# Lista das futuras implementações na aplicação trabalhoJavaVestuario
+
+I1 - Abrir uma janela nova para edição de itens, além de um item de menu com que direciona para a janela de edição.
+
+I2 - Implementar o try/catch para detectar erros na inclusão de itens. CONCLUÍDO
+
+I3 - Colocar uma imagem na tela principal, para torná-la mais amigável.
+
+I4 - Listagem de todas as roupas na lavanderia, guarda-roupa e em uso.
+
+I5 - No menu de criação, possibilidade de escolher que tipo de roupa se trata (social, esporte fino, esporte ou trabalho).
+
+I6 - Janela do menu para consultar roupas
+
+I7 - Botão para sair da aplicação
+
+# Lista de tarefas gerais
+T1 - Assistir a um vídeo de treinamento sobre como desenvolver na interface Java Swing
+
+T2 - Assitir ao vídeo de treinamento acerca do IntelliJ IDEA (vídeo de 1 hora)
--- trabalhoJavaVestuario/src/com/vestuario/main/IncluirRoupa.java
+++ trabalhoJavaVestuario/src/com/vestuario/main/IncluirRoupa.java
@@ -1,16 +1,10 @@
 package com.vestuario.main;
 
-import com.vestuario.classes.LocalRoupa;
 import com.vestuario.classes.Roupa;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class IncluirRoupa {
     public JPanel incluirRoupaPannel;
@@ -27,26 +21,53 @@ public class IncluirRoupa {
     private JLabel precoRoupaLabel;
     private JLabel localRoupaLabel;
     private JComboBox localRoupaComboBox;
-    private JButton Incluir;
+    private JButton incluirButton;
     private JButton cancelarButton;
 
     public IncluirRoupa(JFrame parentFrame) {
 
+//        Adicionar nomes dos locais das roupas no ComboBox
         for (String nomeLocalRoupa : Main.listaLocaisRoupa.keySet()) {
             localRoupaComboBox.addItem(nomeLocalRoupa);
         }
 
-        Incluir.addActionListener(new ActionListener() {
+        incluirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String mensagemErro;
 
-                Main.instanciasRoupasCriadas.add(new Roupa(nomeRoupaTextField.getText(), corRoupaTextField.getText(), Integer.parseInt(numManequimTextField.getText()), tecidoRoupaTextField.getText(), Double.parseDouble(precoRoupaTextField.getText()), Main.listaLocaisRoupa.get(localRoupaComboBox.getSelectedItem())));
+                try {
+// Verificar se não há dados em branco
+                    if (nomeRoupaTextField.getText().isBlank() || corRoupaTextField.getText().isBlank() || numManequimTextField.getText().isBlank() || tecidoRoupaTextField.getText().isBlank() || precoRoupaTextField.getText().isBlank()) {
+                        throw new Exception();
+                    }
 
-                JOptionPane.showMessageDialog(null, "Roupa " + Main.instanciasRoupasCriadas.get(Main.instanciasRoupasCriadas.size() - 1).getNome() + " incluída no " + Main.instanciasRoupasCriadas.get(Main.instanciasRoupasCriadas.size() - 1).getLocalRoupa());
+//                    Tentar ativar o construtor da classe Roupa
+                    Main.instanciasRoupasCriadas.add(new Roupa(nomeRoupaTextField.getText(), corRoupaTextField.getText(), Integer.parseInt(numManequimTextField.getText()), tecidoRoupaTextField.getText(), Double.parseDouble(precoRoupaTextField.getText()), Main.listaLocaisRoupa.get(localRoupaComboBox.getSelectedItem())))
+                    ;
+
+//                    Mostrar caixa de diálogo de sucesso
+                    JOptionPane.showMessageDialog(null, "Roupa " + Main.instanciasRoupasCriadas.get(Main.instanciasRoupasCriadas.size() - 1).getNome() + " incluída no " + Main.instanciasRoupasCriadas.get(Main.instanciasRoupasCriadas.size() - 1).getLocalRoupa());
+                } catch (Exception exception) {
+                    if (nomeRoupaTextField.getText().isBlank() || corRoupaTextField.getText().isBlank() || numManequimTextField.getText().isBlank() || tecidoRoupaTextField.getText().isBlank() || precoRoupaTextField.getText().isBlank()) {
+                        mensagemErro = "Há valores em branco. Favor preencher todos os campos.";
+                    } else if (!numManequimTextField.getText().matches("-?\\d+") && !precoRoupaTextField.getText().matches("-?\\d+\\.?\\d*")) {
+                        mensagemErro = "O valor do número do manequim deve ser um número inteiro e o preço da roupa deve ser um número decimal. Confira os dados digitados e tente novamente.";
+                    } else if (!numManequimTextField.getText().matches("-?\\d+")) {
+                        mensagemErro = "O valor do número do manequim deve ser um número inteiro. Confira os dados digitados e tente novamente.";
+                    } else if (!precoRoupaTextField.getText().matches("-?\\d+\\.?\\d*")) {
+                        mensagemErro = "O valor do preço deve ser um número decimal. Confira os dados digitados e tente novamente.";
+                    } else {
+                        mensagemErro = "Houve erro na inclusão do item. Confira os dados digitados e tente novamente.";
+                    }
+
+                    JOptionPane.showMessageDialog(incluirRoupaPannel, mensagemErro, "Erro", JOptionPane.ERROR_MESSAGE);
+                }
 
             }
 
         });
+
         cancelarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {

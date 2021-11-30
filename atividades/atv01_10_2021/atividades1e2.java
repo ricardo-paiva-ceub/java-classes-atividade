77e68d8fa5c40b9fcd5a06ee21c17db11c947636
ricardo_paiva <ricardo.paiva@sempreceub.com>
2021-11-30 11:58:40 -0300

    Nova atividade inserida - dia 01/10/2021.

---
+++ atividades/atv01_10_2021/atividades1e2.java
@@ -0,0 +1,24 @@
+//# Atividades 1 e 2
+
+//## Classe Carro
+public class Carro {
+    public String marca;
+    private String modelo;
+    private String cor;
+    private String tipoCombustivel;
+    private double capacidadeCombustivel;
+    private double potencia;
+}
+
+public class Pessoa {
+    public String nome
+    private Date dataNascimento
+    private sexo
+}
+
+public class Funcionario {
+    public String nome
+    public String cargo
+    private Date dataNascimento
+    private remuneracao
+}
